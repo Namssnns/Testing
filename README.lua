72f28c3e2fd28a4ebb170f22fd7bbe6cc88ed38c
Namssnns <167390942+Namssnns@users.noreply.github.com>
2024-07-09 16:03:09 -0500

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,5 +1,4 @@
 
-
 local ScreenGui = Instance.new("ScreenGui",game.Players.LocalPlayer.PlayerGui)
 ScreenGui.Name = "Shovel Masters Hack Client"
 
@@ -14,6 +13,7 @@ UiCornerTemp.CornerRadius = UDim.new(0,32)
 
 local UiStrokeTemp = Instance.new("UIStroke")
 UiStrokeTemp.Thickness = 3
+
 
 local GiveWinsFrame = Instance.new("Frame",Main)
 GiveWinsFrame.BackgroundTransparency = 1
@@ -38,6 +38,33 @@ GiveWinsTextBox.Position = UDim2.new(0.492,0,0,0)
 GiveWinsTextBox.Size = UDim2.new(0,200,0,50)
 UiCornerTemp:Clone().Parent = GiveWinsTextBox
 
+local GivePowerFrame = Instance.new("Frame",Main)
+GivePowerFrame.BackgroundTransparency = 1
+GivePowerFrame.Position = UDim2.new(0.068, 0,0.348, 0)
+GivePowerFrame.Size = UDim2.new(0,299,0,57)
+GivePowerFrame.Name = "GivePowerFrame"
+
+local GivePowerButton = Instance.new("TextButton",GivePowerFrame)
+GivePowerButton.Name = "GivePower"
+GivePowerButton.Size = UDim2.new(0,100,0,50)
+GivePowerButton.Position = UDim2.new(0,0,0,0)
+GivePowerButton.Text = "Give Power"
+GivePowerButton.TextScaled = true
+GivePowerButton.Font = Enum.Font.FredokaOne
+GivePowerButton.TextColor3 = Color3.new(255,255,255)
+UiStrokeTemp:Clone().Parent = GivePowerButton
+UiCornerTemp:Clone().Parent = GivePowerButton
+
+local GivePowerTextBox = Instance.new("TextBox",GivePowerFrame)
+GivePowerTextBox.Name = "GivePowerTextBox"
+GivePowerTextBox.Position = UDim2.new(0.492,0,0,0)
+GivePowerTextBox.Size = UDim2.new(0,200,0,50)
+UiCornerTemp:Clone().Parent = GivePowerTextBox
+
+
+
+
+
 
 GiveWinsButton.MouseButton1Click:Connect(function()
 	if GiveWinsTextBox.Text then
@@ -48,6 +75,22 @@ GiveWinsButton.MouseButton1Click:Connect(function()
 
 		game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("changeWins"):FireServer(unpack(args))
 
+
+
+	end
+end)
+
+
+
+
+GivePowerButton.MouseButton1Click:Connect(function()
+	if GivePowerTextBox.Text then
+		local args = {
+			[1] = GivePowerTextBox.Text,
+			[2] = 302984652537821
+		}
+
+		game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("changePower"):FireServer(unpack(args))
 
 
 	end
